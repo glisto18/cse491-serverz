353511078901ac96396d4230665bbfdab99e68f0
Daniel Roman <romanda1@arctic.cse.msu.edu>
2014-02-19 22:57:16 -0500

    Cloned for resubmission

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import StringIO
 import jinja2
 import sys
 
+<<<<<<< HEAD
 # Set up the loader and environment
 # for jinja2 templating engine
 loader = jinja2.FileSystemLoader('./templates')
@@ -161,3 +162,78 @@ def main():
 
 if __name__ == '__main__':
     main()
+=======
+def main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
+	
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    s.listen(5)                 # Now wait for client connection.
+	
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.    
+        c, (client_host, client_port) = s.accept()
+	handle_connection(c)
+
+        print 'Got connection from', client_host, client_port
+
+def handle_connection(c):
+    req = c.recv(1000)
+    req = req.split(' ')
+    reqType = req[0]
+    path = req[1]
+    if reqType == 'GET':
+        handle_get(c,path)
+    elif reqType == 'POST':
+        handle_post(c)
+    c.close()
+
+def handle_get(c,path):	   
+    # send a response
+    #if main directory
+    if path == '/':
+        c.send('HTTP/1.0 200 OK\r\n' + \
+               'Content-type: text/html\r\n' + \
+               '\r\n')
+	c.send('<html><body>' + \
+               '<h1>Hello, world.</h1>' + \
+               "This is romanda1's Web server written by J. Rucinski. <BR>" + \
+	       "<A HREF='/content'>Content</A><BR>" + \
+               "<A HREF='/file'>File</A><BR>" + \
+	       "<A HREF='/image'>Image</A>" + \
+	       '</html></body>')
+
+    #if content directory
+    elif path == '/content':
+        c.send('HTTP/1.0 200 OK\r\n' + \
+               'Content-type: text/html\r\n' + \
+	       '\r\n' + \
+	       '<h1>Content Page</h1>')
+    #if file directory
+    elif path == '/file':
+        c.send('HTTP/1.0 200 OK\r\n' + \
+               'Content-type: text/html\r\n' + \
+	       '\r\n' + \
+	       '<h1>File Page</h1>')
+    #if image directory
+    elif path == '/image':
+        c.send('HTTP/1.0 200 OK\r\n' + \
+               'Content-type: text/html\r\n' + \
+	       '\r\n' + \
+	       '<h1>Image Page</h1>')
+
+def handle_post(c):
+    c.send('HTTP/1.0 200 OK\r\n' + \
+           'Content-type: text/html\r\n' + \
+	   '\r\n' + \
+	   '<h1>Hello, world.</h1>' + \
+	   "This is romanda1's Web server written by J. Rucinski. \r\n" + \
+	   'This is a POST.')
+
+if __name__== '__main__':
+	main()
+>>>>>>> f0618a0f39c17ced66537faaa83dcdc0997a32f0
--- test_server.py
+++ test_server.py
@@ -30,6 +30,65 @@ class FakeConnection(object):
 
 def test_handle_connection():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
+<<<<<<< HEAD
+=======
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+		      '<html><body>' + \
+                      '<h1>Hello, world.</h1>' + \
+                      "This is romanda1's Web server written by J. Rucinski. <BR>" + \
+		      "<A HREF='/content'>Content</A><BR>" + \
+		      "<A HREF='/file'>File</A><BR>" + \
+		      "<A HREF='/image'>Image</A>" + \
+		      '</html></body>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>Content Page</h1>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>File Page</h1>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>Image Page</h1>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_post():
+    conn = FakeConnection("POST /file HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>Hello, world.</h1>' + \
+		      "This is romanda1's Web server written by J. Rucinski. \r\n" + \
+		      'This is a POST.'
+>>>>>>> f0618a0f39c17ced66537faaa83dcdc0997a32f0
 
     server.handle_connection(conn)
 
